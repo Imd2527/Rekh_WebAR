2636484d332cc6cb085005eeb80fc0f29334563c
Imd2527 <athenianwarrior1989@gmail.com>
2026-08-20 20:45:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,17 @@ function showScreen(screenId) {
 setTimeout(() => {
   showScreen("welcome");
 }, 2500);
+const storyFrames = [
+  "assets/images/Rekh_How_It_Works_Frame_1_HD.png",
+  "assets/images/Rekh_How_It_Works_Frame_2_HD.png",
+  "assets/images/Rekh_How_It_Works_Frame_3_HD.png",
+  "assets/images/Rekh_How_It_Works_Frame_4_HD.png"
+];
+
+function showStory(index) {
+  document.getElementById("storyFrame").src = storyFrames[index];
+
+  document.querySelectorAll(".dot").forEach((dot, i) => {
+    dot.classList.toggle("active", i === index);
+  });
+}
